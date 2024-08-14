26cd0cb684a557db6a2d939d0465362bbd058357
patrickpiedad <patrickpiedad@gmail.com>
2024-08-14 20:57:06 +0200

    Added ProductofArrayExceptSelf LeetCode Problem 238.

---
+++ LeetCode-CSharp/LongestCommonPrefix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp
+{
+    internal class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixMethod(string[] strs)
+        {
+            foreach(string element in strs)
+            {
+                
+            }
+        }
+    }
+}
