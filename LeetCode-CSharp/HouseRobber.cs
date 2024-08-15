03c08d53a10b0df893de1fca9b8661b4307aea70
patrickpiedad <patrickpiedad@gmail.com>
2024-08-15 19:22:20 +0200

    Added HouseRobber LeetCode 198.

---
+++ LeetCode-CSharp/HouseRobber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp
+{
+    internal class HouseRobber
+    {
+        static int MaxMoney(int[] houseMonies, int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            int val1 = houseMonies[0];
+
+            if (n == 1)
+            {
+                return val1;
+            }
+
+            int val2 = Math.Max(houseMonies[0], houseMonies[1]);
+            if (n == 2)
+            {
+                return val2;
+            }
+
+            int maxVal = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                maxVal = Math.Max(houseMonies[i] + val1, val2);
+                val1 = val2;
+                val2 = maxVal;
+            }
+
+            return maxVal;
+        }
+    }
+}
