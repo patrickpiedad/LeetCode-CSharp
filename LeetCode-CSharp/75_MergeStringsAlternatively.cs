1ccc9caf586d1a8df580e9b92c2d1d620e84eec4
patrickpiedad <patrickpiedad@gmail.com>
2024-09-14 19:13:39 +0200

    refactor. adding new attempt to LC75 Merge Strings Alternately.

--- LeetCode-CSharp/75_MergeStringsAlternatively.cs
+++ LeetCode-CSharp/75_MergeStringsAlternatively.cs
@@ -42,6 +42,44 @@ namespace LeetCode_CSharp
 
             return builder.ToString();
         }
+        
+        public string MergeAlternately(string word1, string word2) {
+            // Need to take two strings and make into one.
+            // Need to take into account that one maybe longer than other, avoid out of bounds error
+            // Start with word 1, use stringbuilder
+            // Use 1x for loop, use if conditional to stop adding shorter array
+            // Need to know which word is shorter and actual length
+
+
+            string longWord = "";
+            string shortWord = "";
+
+            if (word1.Length > word2.Length){
+                longWord = word1;
+                shortWord = word2;
+            }
+
+            else{
+                longWord = word2;
+                shortWord = word1;
+            }
+
+            StringBuilder answer = new StringBuilder();
+
+            for (int i = 0; i < longWord.Length; i++){
+                if (i < shortWord.Length){
+                    answer.Append(word1[i]);
+                    answer.Append(word2[i]);
+                }
+
+                else{
+                    answer.Append(longWord[i]);
+                }
+
+            }
+
+            return answer.ToString();    
+        }
 
     }
 }
