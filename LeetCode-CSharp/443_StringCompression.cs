93ec4f242c485f02203818e2d5b6cc27da6cfe31
patrickpiedad <patrickpiedad@gmail.com>
2024-09-24 23:31:41 +0200

    feat. add LC443 String Compression

---
+++ LeetCode-CSharp/443_StringCompression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp
+{
+    internal class StringCompression
+    {
+        public int Compress(char[] chars)
+        {
+
+            int currIndex = 0;
+            int i = 0;
+
+            while (i < chars.Length)
+            {
+                int count = 0;
+                char currChar = chars[i];
+
+                while (i < chars.Length && chars[i] == currChar)
+                {
+                    i++;
+                    count++;
+                }
+
+                chars[currIndex] = currChar;
+                currIndex++;
+
+                if (count > 1)
+                {
+                    foreach (char c in count.ToString())
+                    {
+                        chars[currIndex] = c;
+                        currIndex++;
+                    }
+                }
+            }
+            return currIndex;
+        }
+
+    }
+}
