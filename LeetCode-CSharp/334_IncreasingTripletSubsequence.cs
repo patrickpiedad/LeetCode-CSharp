82bfc0dd52a42d94caff7bd70ad7b18d8afdf91c
patrickpiedad <patrickpiedad@gmail.com>
2024-09-24 22:53:47 +0200

    refactor. remade LC 334 Increasing Triplet Subsequence with cleaner code

--- LeetCode-CSharp/334_IncreasingTripletSubsequence.cs
+++ LeetCode-CSharp/334_IncreasingTripletSubsequence.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LeetCode_CSharp
 {
-    internal class _334_IncreasingTripletSubsequence
+    internal class IncreasingTripletSubsequence
     {
 
         public static bool IncreasingTriplet(int[] nums)
@@ -31,6 +31,20 @@ namespace LeetCode_CSharp
                 {
                     return true;
                 }
+            }
+            return false;
+        }
+
+        public static bool IncreasingTriplet2(int[] nums)
+        {
+            int first = int.MaxValue;
+            int second = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < first) first = nums[i];
+                else if (nums[i] < second) second = nums[i];
+                else return true;
             }
             return false;
         }
