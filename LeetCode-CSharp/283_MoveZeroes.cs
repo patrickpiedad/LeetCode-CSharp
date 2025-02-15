db0e6af8661a5234f233c6681c7b74be31ea8264
patrickpiedad <patrickpiedad@gmail.com>
2025-02-15 07:31:54 +0100

    solved LC283 Move Zeroes

--- LeetCode-CSharp/283_MoveZeroes.cs
+++ LeetCode-CSharp/283_MoveZeroes.cs
@@ -78,4 +78,24 @@ public static class MoveZeroes
             }
         }
     }
+    
+    
+    public static void MoveZeroes4(int[] nums) {
+        // [0,1,0,3,12]
+
+        int insert = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[insert];
+                nums[insert] = temp;
+                insert++;
+            }
+        }
+    }
+    
+    
 }
