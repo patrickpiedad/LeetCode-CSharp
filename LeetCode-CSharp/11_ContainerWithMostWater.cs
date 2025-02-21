a5bc42521b6e25939b34bed7794f1df3af753bd9
patrickpiedad <patrickpiedad@gmail.com>
2025-02-21 06:30:31 +0100

    solved LC11 Container With Most Water

---
+++ LeetCode-CSharp/11_ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+namespace LeetCode_CSharp;
+
+public static class ContainerWithMostWater {
+    public static int MaxArea(int[] height) {
+        // Input: height = [1,8,6,2,5,4,8,3,7]
+        // Output: 49
+        // Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+        // In this case, the max area of water (blue section) the container can contain is 49.
+        
+        // Looking for optimal point where x distance is maximized while y is maximized
+        // Greedy, make the best known decision at each point
+        
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right)
+        {
+            // area calculation
+            int currentArea = Math.Min(height[left], height[right]) * (right - left);
+            maxArea = Math.Max(maxArea, currentArea);
+
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+
+        return maxArea;
+    }
+}
